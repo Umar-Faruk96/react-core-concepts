d1df9eb055fa0f60e78d906f1a21f9feb967ed8d
Omor Faruk Sheikh Raihan <omorfarukjabbar@gmail.com>
2025-07-30 18:10:04 +0600

    work on course component

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,19 +3,19 @@ import React from "react";
 // ~ Courses Component
 const Courses = () => {
   return (
-    <div>
-      <h2 style={{ textDecoration: "underline double green" }}>
-        Course Component
-      </h2>
-      <Course subject="React Learning" topic="Component Creation"></Course>
-      <Course
-        subject="React Learning"
-        topic="Component Creation Dynamically"
-      ></Course>
-      <Course
-        subject="React Learning"
-        topic="Dynamic Component Creation with style"
-      ></Course>
+    <div style={{ marginBlock: "1rem" }}>
+      <h2>Course Creation</h2>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "0.6rem", marginTop: "0.8rem" }}>
+        <Course subject="React 18 Learning" topic="Component Creation"></Course>
+        <Course
+          subject="React 18 Learning"
+          topic="Component Creation with Inline Style"
+        ></Course>
+        <Course
+          subject="React 18 Learning"
+          topic="Dynamic Component Creation with Style Object"
+        ></Course>
+      </div>
     </div>
   );
 };
@@ -24,11 +24,11 @@ const Courses = () => {
 const Course = (props) => {
   // ? create style object to use css
   const styling = {
-    border: "2px solid red",
+    border: "2px solid #f62ed1",
     padding: "0.5rem",
     borderRadius: "0.2rem",
     margin: "0.4rem",
-    width: "50vw",
+    width: "50%",
   };
   // console.log(props);
   return (
